4c57ce77a96e4a22186a78cafd9452c9c3d71d1d
somefoo <50917034+somefoo@users.noreply.github.com>
2019-11-17 15:29:02 +0100

    Setup test program

--- sort/include/radix_sort.hpp
+++ sort/include/radix_sort.hpp
@@ -3,11 +3,25 @@
 #include <cstdint>
 #include <cstring>
 #include <functional>
+
+#include <iostream>
+#include <cassert>
+#include <type_traits>
+#include <typeinfo>
+
 namespace rdx{
+  namespace {
+     
+  }
 template <typename Iterator, typename Comparator>
-static inline void radix_sort(Iterator begin, Iterator end, Comparator comp, size_t MaxDepth) {
-  //typedef std::less<typename std::iterator_traits<Iterator>::value_type> Comparator;
+static inline void radix_sort(const Iterator begin, const Iterator end, const Comparator comp) {
+  typedef typename std::iterator_traits<Iterator>::value_type Type;
+  constexpr const size_t window_size = 1 << 2;
+  constexpr const size_t window_count = (sizeof(Type) * 8) / window_size;
+  const size_t element_count = (end - begin);
+
+  std::cout << "window_count:" << window_count << " window_size:" << window_size << " element_count:" << element_count << '\n';
+
   std::sort(begin, end, comp);
-  (void) MaxDepth;
 }
 }
--- tests/radix_sort_fast_test.cpp
+++ tests/radix_sort_fast_test.cpp
@@ -1,20 +1,46 @@
-#include <radix_sort.hpp>
 #include <array>
+#include <radix_sort.hpp>
+struct key_data_pair {
+  key_data_pair() {
+    static uint32_t up_counter = 0;
+    key = up_counter % 59159;
+    up_counter += 163417;
+  }
+  uint32_t key;
+  uint32_t data[11];
+};
 
 template <typename Iterator, typename Comparator>
-int is_sorted(Iterator begin, Iterator end, Comparator comp){
-  for(Iterator start = begin; start < end; ++start){
-    if(!comp(*begin, *end)) return 1;
+bool is_sorted(const Iterator begin, const Iterator end, const Comparator comp) {
+  for (Iterator current = begin; current < end - 1; ++current) {
+    if (!comp(*current, *(current + 1))) return false;
   }
+  return true;
+}
+
+int main() {
+  std::array elements{43, 515, 645, 3, 12, 5, 64523, 5423, 43, 6454};
+  auto comp = [](const int& a, const int& b) { return a <= b; };
+  std::array<key_data_pair, 10> elements_pair;
+  auto comp_pair = [](const key_data_pair& a, const key_data_pair& b) {return a.key <= b.key;};
+
+  rdx::radix_sort(elements.begin(), elements.end(), comp);
+  rdx::radix_sort(elements_pair.begin(), elements_pair.end(), comp_pair);
+
+  bool sorted = is_sorted(elements.begin(), elements.end(), comp);
+  if(sorted){
+    std::cout << "[SUCCESS] Sorted small integer array.\n"; 
+  }else{
+    return 1;
+  }
+
+  sorted = is_sorted(elements_pair.begin(), elements_pair.end(), comp_pair);
+  if(sorted){
+    std::cout << "[SUCCESS] Sorted small struct (key,value) array.\n"; 
+  }else{
+    return 1;
+  }
+
+
   return 0;
 }
-
-int main(){
-  std::array elements{43,515,645,3,12,5,64523,5423,43,6454,43};
-  auto comp = [](const int& a, const int& b){ return a<b;};
-
-  rdx::radix_sort(elements.begin(), elements.end(), comp, 32);
-
-  return is_sorted(elements.begin(), elements.end(), comp);
-}
-
