d3377083bacb78a19a170d66c1df2b0bf65fff43
somefoo <50917034+somefoo@users.noreply.github.com>
2019-11-23 12:06:08 +0100

    Store large array of heap (std::vector vs std::array)

--- tests/radix_sort_fast_test.cpp
+++ tests/radix_sort_fast_test.cpp
@@ -1,4 +1,5 @@
 #include <array>
+#include <vector>
 #include <radix_sort.hpp>
 struct key_data_pair {
   key_data_pair() {
@@ -10,40 +11,38 @@ struct key_data_pair {
   uint32_t data[11];
 };
 
-template <typename Iterator, typename Comparator>
-bool is_sorted(const Iterator begin, const Iterator end, const Comparator comp) {
-  for (Iterator current = begin; current < end - 1; ++current) {
-    if (!comp(*current, *(current + 1))) return false;
-  }
-  return true;
-}
+//template <typename Iterator, typename Comparator>
+//bool is_sorted(const Iterator begin, const Iterator end, const Comparator comp) {
+//  for (Iterator current = begin; current < end - 1; ++current) {
+//   if (!comp(*current, *(current + 1))) return false;
+//  }
+//  return true;
+//}
 
 int main() {
   std::array<uint32_t,10> elements{43, 515, 645, 3, 12, 5, 64523, 5423, 43, 6454};
-  std::array<key_data_pair, 100000> elements_pair;
+  std::vector<key_data_pair> elements_pair(1000000);
 
-  auto comp = [](const uint32_t a, const uint32_t b) { return a <= b; };
-  auto comp_pair = [](const key_data_pair& a, const key_data_pair& b) {return a.key <= b.key;};
+  auto comp = [](const uint32_t a, const uint32_t b) { return a < b; };
+  auto comp_pair = [](const key_data_pair& a, const key_data_pair& b) {return a.key < b.key;};
 
   rdx::radix_sort(elements.begin(), elements.end(), comp);
   rdx::radix_sort(elements_pair.begin(), elements_pair.end(), comp_pair);
 
-  bool sorted = is_sorted(elements.begin(), elements.end(), comp);
+  bool sorted = std::is_sorted(elements.begin(), elements.end(), comp);
   if(sorted){
     std::cout << "[SUCCESS] Sorting small integer array.\n"; 
   }else{
     std::cout << "[FAILED] Sorting small integer array.\n"; 
     return 1;
   }
-
-  sorted = is_sorted(elements_pair.begin(), elements_pair.end(), comp_pair);
+  sorted = std::is_sorted(elements_pair.begin(), elements_pair.end(), comp_pair);
   if(sorted){
     std::cout << "[SUCCESS] Sorting small struct (key,value) array.\n"; 
   }else{
     std::cout << "[FAILED] Sorting small integer array.\n"; 
     return 1;
   }
-
 
   return 0;
 }
