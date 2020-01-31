48f0ce672dc673ca678422664e184df483c98d66
somefoo <50917034+somefoo@users.noreply.github.com>
2020-01-31 13:39:21 +0100

    Added a short version of the prefix snake version

--- sort/include/radix_sort_prefix.hpp
+++ sort/include/radix_sort_prefix.hpp
@@ -212,7 +212,98 @@ static inline void radix_sort_prefix_par_nibble(const Iterator begin, const Iter
   }
 }
 
+template <typename Iterator, typename KeyGetter>
+static inline void radix_sort_prefix_par_short(const Iterator begin, const Iterator end,
+                                  const KeyGetter key_getter) {
+  TIME_START();
 
+  //Setup
+  const size_t thread_count = omp_get_max_threads();
+  typedef typename std::iterator_traits<Iterator>::value_type data_type;
+  constexpr const size_t size_of_key = sizeof(key_getter(*begin));
+  const size_t element_count = std::distance(begin, end);
+
+  //The key cache contains the key value for the current radix
+  //iteration
+  std::unique_ptr<uint16_t[]> key_cache(new uint16_t[element_count]);
+  //Data cache, a buffer which will be used to write the result of a
+  //radix step into. Notice, impl. is out of place.
+  std::unique_ptr<data_type[]> data_cache(new data_type[element_count]);
+
+  //We use pointers internally; we don't have concepts yet...
+  data_type* begin_original = &*begin;
+  data_type* end_original = &*end;
+  data_type* begin_cache = data_cache.get();
+  data_type* end_cache = &(data_cache[element_count - 1]);
+
+  TIME_PRINT_RESET("Setup time");
+
+  //2D array holding the bucket sizes for each thread
+  std::vector<std::array<size_t, 65536>> bucket_sizes(thread_count, {0});
+
+  //Start of actual work//////////////////
+  for (size_t depth = 0; depth < size_of_key/2; ++depth) {
+    //create the key cache
+    //static schedule to minimise false sharing
+    #pragma omp parallel for schedule(static) 
+    for (size_t i = 0; i < element_count; ++i) {
+      auto key = key_getter(*(begin_original + i)); 
+      key_cache[i] = reinterpret_cast<uint16_t*>(&key)[depth];
+    }
+    TIME_PRINT_RESET("Create Cache");
+
+    //eval. the bucket sizes for each thread
+    #pragma omp parallel
+    {
+      std::array<size_t, 65536> private_bucket_size{0};  // Init to 0
+      #pragma omp for schedule(static)
+      for (size_t i = 0; i < element_count; ++i) {
+        ++private_bucket_size[key_cache[i]];
+      }
+
+      bucket_sizes[omp_get_thread_num()] = std::move(private_bucket_size);
+    }
+    TIME_PRINT_RESET("Find bucket size");
+    
+    //Snake prefix sum
+    std::vector<std::array<data_type*, 65536>> buckets(thread_count, {0});
+    buckets[0][0] = begin_cache;
+    for(size_t index = 0; index < 65536; ++index){
+      for(size_t thread = 1; thread < thread_count; ++thread){
+        buckets[thread][index] = buckets[thread - 1][index] + bucket_sizes[thread - 1][index];
+        //std::cout << "i:" << index << " t:" << thread << " at: " <<(uint64_t) (buckets[thread][index] - begin_cache) << '\n';
+      }
+      if(index < 65535){
+        buckets[0][index + 1] = buckets[thread_count - 1][index] + bucket_sizes[thread_count - 1][index];
+      }
+    }
+    TIME_PRINT_RESET("Create initial buckets");
+
+    //Redistribute the data
+    #pragma omp parallel
+    {
+      //Make local copy, to minimise false sharing at the boundaries 
+      //note, std::move would not prevent?
+      std::array<data_type*, 65536> bucket_local = buckets[omp_get_thread_num()];
+      #pragma omp for schedule(static)
+      for(size_t i = 0; i < element_count; ++i){
+        *(bucket_local[key_cache[i]]++) = std::move(*(begin_original + i));
+      }	
+    }
+    TIME_PRINT_RESET("Redistribute data");
+
+    //We could actually be much faster with a swap (two moves), but I need the
+    //whole object not just iterators.
+    std::swap(begin_original, begin_cache);
+    std::swap(end_original, end_cache);
+  }
+  //End of actual work//////////////////////
+
+  //If number of iterations was odd (we need to copy)
+  if(size_of_key & 1){
+    std::move(data_cache.get(), data_cache.get() + element_count, begin);
+  }
+}
 
 
 }  // namespace rdx
