7e782229cbd620366bb102e45284ec9d679a9607
Pit Henrich <50917034+somefoo@users.noreply.github.com>
2020-02-06 10:22:53 +0100

    Added comment and copyright to helper file

--- sort/debug_helper.hpp
+++ sort/debug_helper.hpp
@@ -1,3 +1,21 @@
+
+/*******************************************************************************
+ * sort/debug_helper.hpp
+ *
+ * Helper DEFINES for timing functions and sections.
+ *   
+ * Copyright (C) 2020 by Pit Henrich <pithenrich2d@gmail.com>
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION
+ ******************************************************************************/
+
 #pragma once
 #include <chrono>
 
